e693ee7a8469bd7947426ea464eadf6e320913af
Jeff Calder <jcalder@umn.edu>
2024-07-18 22:43:18 -0500

    Updates

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ for e in range(episodes):
     state = env.reset()
 
     # Play the game!
-    while True:
+    while False:
 
         # 3. Show environment (the visual) [WIP]
         # env.render()
